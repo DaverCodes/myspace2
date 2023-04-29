e3067ac3fc7cd06c04979e99c611c54375ad8cd3
daveS <davesimpcontrol@gmail.com>
2023-04-29 10:55:23 -0600

    adjusted index and filled out base schema for users

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
+
+
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
