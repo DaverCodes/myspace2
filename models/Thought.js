6d2753430e6bae2997e66cd0fa97855cf9eb8f59
daveS <davesimpcontrol@gmail.com>
2023-04-29 11:14:37 -0600

    thought and reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -2,44 +2,69 @@ const { Schema, model } = require('mongoose');
 const Tag = require('./Tag');
 
 // Schema to create Post model
-const thoughtSchema = new Schema(
+const reactionSchema = new Schema(
   {
-    published: {
-      type: Boolean,
-      default: false,
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
       default: Date.now,
+      // Use a getter method to format timestamp on query
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
-    buildSuccess: {
-      type: Boolean,
-      default: true,
+  },
+  {
+    toJSON: {
+      getters: true,
     },
-    description: {
+  }
+);
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
       type: String,
-      minLength: 15,
-      maxLength: 500,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
     },
-    tags: [Tag],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Use a getter method to format timestamp on query
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
   {
     toJSON: {
       virtuals: true,
+      getters: true,
     },
     id: false,
   }
 );
-
 // Create a virtual property `getTags` that gets the amount of tags associated with an Thought
-thoughtSchema
-  .virtual('getResponses')
-  // Getter
-  .get(function () {
-    return this.tags.length;
-  });
+ThoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
 
-// Initialize our Thought model
-const Thought = model('Thought', thoughtSchema);
 
-module.exports = Thought;
+const Thought = mongoose.model('Thought', ThoughtSchema);
+
+module.exports = { Thought, reactionSchema };
