f08a52882735a4fbb9292e3931f3ddfaf210eb92
daveS <davesimpcontrol@gmail.com>
2023-04-30 11:26:59 -0600

    wow big push

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
-
+const cwd = process.cwd();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
-
 
 router.use('/api', apiRoutes);
 
