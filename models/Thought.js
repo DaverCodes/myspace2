5375087a31d137fa037f2cac28b0ce0c7acf7922
daveS <davesimpcontrol@gmail.com>
2023-04-30 18:50:44 -0600

    re read the readme, reaction schema added back to the thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,38 @@
 const { Schema } = require('mongoose');
 
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Use a getter method to format timestamp on query
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+// Create a virtual property `getTags` that gets the amount of tags associated with an Thought
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
 
 
 const reactionSchema = new Schema(
@@ -34,42 +67,7 @@ function dateFormat(date) {
   return Intl.DateTimeFormat('en-US').format(date);
 }
 
-module.exports = { User, Thought, reactionSchema };
-
-
-const thoughtSchema = new Schema(
-  {
-    thoughtText: {
-      type: String,
-      required: true,
-      minlength: 1,
-      maxlength: 280,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      // Use a getter method to format timestamp on query
-      get: (createdAtVal) => dateFormat(createdAtVal),
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    reactions: [reactionSchema],
-  },
-  {
-    toJSON: {
-      virtuals: true,
-      getters: true,
-    },
-    id: false,
-  }
-);
-// Create a virtual property `getTags` that gets the amount of tags associated with an Thought
-thoughtSchema.virtual('reactionCount').get(function () {
-  return this.reactions.length;
-});
-
+module.exports = {reactionSchema};
 
 const Thought = mongoose.model('Thought', thoughtSchema);
 
